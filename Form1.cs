fd0a56c7869576c8423a4208b88f33d7b53a0a0e
MuhammedKaplan0 <100554179+MuhammedKaplan0@users.noreply.github.com>
2022-03-22 11:05:36 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VeriDogrulamaUygulamasi2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ErrorProvider ep = new ErrorProvider();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            int sonuc;
+            if(int.TryParse(textBox1.Text,out sonuc))
+        {
+           ep.SetError(textBox1, "");
+        }
+         else        
+         
+          e.Cancel = true;
+          ep.SetError(textBox1, "Numara girişi hatalı");
+         }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox2.Text == "")
+        {
+            e.Cancel = true;
+            ep.SetError(textBox2, "Adı ve soyadı giriniz.");
+        }
+            else
+        {
+            ep.SetError(textBox2, "");
+        }
+        }
+
+        private void textBox3_Validating(object sender, CancelEventArgs e)
+        {
+            int dersNotu;
+        if(int.TryParse(textBox3.Text,out dersNotu))
+        {
+        if(dersNotu<0 || dersNotu>100)
+        {
+        e.Cancel = true;
+        ep.SetError(textBox3, "0-100 arasında değer giriniz.");
+        }
+        else
+        {
+        ep.SetError(textBox3, "");
+        }
+        }
+        else
+        {
+        e.Cancel = true;
+        ep.SetError(textBox3, "Sayısal değer giriniz.");
+        }
+        }
+        }
+    }
+
